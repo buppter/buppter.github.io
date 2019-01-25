516c43ca7b34c885103809aae85fed6a6fcc2722
sxt <shixintian@bupt.edu.cn>
2019-01-25 17:37:59 +0800

    Site updated: 2019-01-25 17:37:59

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 24 2019 22:56:01 GMT+0800 (中国标准时间)
+// build time:Fri Jan 25 2019 14:49:44 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
